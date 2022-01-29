6200b10bcf83aff6c892020cdf274715711bf6f6
Aleks164 <alekseylehamail.ru>
2022-01-29 19:23:48 +0300

    5'th done

--- src/5ex.ts
+++ src/5ex.ts
@@ -3,13 +3,14 @@ const orderStates = [
   "inWork",
   "buyingSupplies",
   "producing",
-  "fullfilled"
+  "fullfilled",
 ] as const;
 
 type OrderState = typeof orderStates[number];
 
 type FIXME = Exclude<OrderState, "buyingSupplies" | "producing">;
 
+// eslint-disable-next-line no-shadow
 export const getUserOrderStates = (orderStates: OrderState[]): FIXME[] =>
   orderStates.filter(
     (state): state is FIXME =>
